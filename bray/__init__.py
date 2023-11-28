f5ff95b4474937430411e413d49b370f2fa3f427
pengyao04 <pengyao04@corp.netease.com>
2023-11-28 16:14:42 +0800

    refine some code

--- bray/__init__.py
+++ bray/__init__.py
@@ -16,7 +16,11 @@ from bray.model.onnx import export_onnx
 from bray.trainer.trainer import RemoteTrainer
 from bray.actor.actor import RemoteActor
 from bray.actor.base import Actor
-from bray.utils.nested_array import NestedArray, handle_nested_array, make_batch
+from bray.utils.nested_array import (
+    NestedArray,
+    handle_nested_array,
+    make_batch,
+)
 from bray.metric.metric import (
     merge,
     query,
@@ -29,34 +33,35 @@ from bray.metric.metric import (
 )
 from bray.metric.validate import Metric
 
-import ray, logging
+import ray, logging, os
 
 logger = logging.getLogger("ray")
+
+
+def init(project: str, trial: str, **kwargs):
+    """
+    初始化 bray 运行环境，内部封装了 ray.init ，请在调用任何
+      bray 的API之前调用此函数
+    Args:
+        project: 项目名称，也就是项目的根目录
+        trial: 试验名称，和 project 一起拼接为试验的根目录
+        **kwargs: ray.init 的关键字参数
+    """
+    trial_path = os.path.join(project, trial)
+    trial_path = os.path.abspath(trial_path)
+
+    if not os.path.exists(trial_path):
+        os.makedirs(trial_path)
+
+    ray.init(namespace=trial_path, dashboard_host="0.0.0.0", **kwargs)
+
+    # 启动 RemoteMetrics 保证指标输出到Driver节点
+    get_metrics_worker()
+
+    print("bray init success with path: ", trial_path)
 
 
 def run_until_asked_to_stop():
     import signal
 
     signal.sigwait([signal.SIGTERM, signal.SIGINT])
-
-
-def init(project: str, trial: str, **kwargs):
-    """
-    初始化 bray 运行环境，内部封装了 ray.init ，请在调用任何 bray 的API之前调用此函数
-    Args:
-        project: 项目名称，也就是项目的根目录
-        trial: 试验名称，和 project 一起拼接为试验的根目录
-        **kwargs: ray.init 的参数
-    """
-    import os
-
-    trial_path = os.path.join(project, trial)
-    trial_path = os.path.abspath(trial_path)
-    if not os.path.exists(trial_path):
-        os.makedirs(trial_path)
-
-    ray.init(namespace=trial_path, dashboard_host="0.0.0.0", **kwargs)
-
-    get_metrics_worker()  # 启动 RemoteMetrics 保证指标输出到Driver节点
-
-    print("bray init success with path: ", trial_path)
