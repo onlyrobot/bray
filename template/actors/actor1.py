f0f5a2dd9e12bb2db4282a692e2c7ee5a517d5f3
pengyao04 <pengyao04@corp.netease.com>
2023-12-26 21:12:50 +0800

    add actor example

---
+++ template/actors/actor1.py
@@ -0,0 +1,15 @@
+import bray
+
+
+class Actor1(bray.Actor):
+    def __init__(self):
+        raise NotImplementedError
+
+    async def start(self, game_id, data: bytes) -> bytes:
+        raise NotImplementedError
+
+    async def tick(self, data: bytes) -> bytes:
+        raise NotImplementedError
+
+    async def end(self, data: bytes) -> bytes:
+        raise NotImplementedError
